a5b5cc627bda52ad1bc234090d3bea81e60a9a06
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-03-05 21:28:56 -0600

    chapter 13 finished

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,44 +1,76 @@
 package org.launchcode.codingevents.models;
 
 import java.util.Objects;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 12, message = "Name must be between 3 and 12 chars long")
     private String name;
+
+    @Size(max = 50, message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email. Try again")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
 
+    public Event(){
+
+    }
+
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
     }
 
+    public String getContactEmail() {
+
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+
+        this.contactEmail = contactEmail;
+    }
+
     public int getId() {
+
         return id;
     }
 
     @Override
     public String toString() {
+
         return name;
     }
 
